1062f6d1b8348052dc55edb6c12b07df3a318a34
majormaxx <emersondaniel300@gmail.com>
2025-04-17 23:35:14 +0100

    in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => (
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
           src={logo}
-          alt="hobank"
+          alt="hoobank"
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
+          A new way to make the payments easy, reliable and secure
         </p>
       </div>
 
