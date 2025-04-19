4814982c3660a260004bc7edb50658793e4a5b0d
majormaxx <emersondaniel300@gmail.com>
2025-04-19 09:32:34 +0100

    correction made

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import styles from "../style";
-import {discount,robot} from "../assets";
+import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 
-const Hero = () => {
-  return (
+const Hero = () => 
+  (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
@@ -49,6 +49,6 @@ const Hero = () => {
       </div>
     </section>
   );
-};
+
 
 export default Hero;
